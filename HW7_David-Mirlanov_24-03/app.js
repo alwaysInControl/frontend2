6afc7f85bcd91d413aadba4cccf72ed1c7a51415
alwaysInControl <you@example.com>
2023-03-28 23:26:59 +0600

    task7

---
+++ HW7_David-Mirlanov_24-03/app.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll('.showAnswer')
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling
+        if (answer.style.display === 'none') {
+            answer.style.display = 'block'
+            button.textContent = 'Скрыть ответ'
+        } else {
+            answer.style.display = 'none'
+            button.textContent = 'Показать ответ'
+        }
+    })
+})
